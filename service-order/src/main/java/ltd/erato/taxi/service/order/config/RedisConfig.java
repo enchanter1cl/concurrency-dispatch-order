ae3531f59b5baff2c7c39973a9b9f5f168be821d
zhangYuan <zhangyuan@authing.cn>
2023-06-05 13:00:24 +0800

    chore: redis 配置代码优化

--- service-order/src/main/java/ltd/erato/taxi/service/order/config/RedisConfig.java
+++ service-order/src/main/java/ltd/erato/taxi/service/order/config/RedisConfig.java
@@ -3,16 +3,28 @@ package ltd.erato.taxi.service.order.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisConfig {
 
+    private String protocol = "redis://";
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://121.37.71.228:6380").setPassword("foobared").setDatabase(4);
+        config.useSingleServer().setAddress(protocol+ host + ":" + port).setPassword(password).setDatabase(4);
         return Redisson.create(config);
     }
 }
